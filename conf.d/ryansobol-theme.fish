bfa0a0713355a562be45363b6038d2195cdc1197
Ryan Sobol <hello@ryansobol.com>
2023-10-24 15:03:00 -0700

    initial

---
+++ conf.d/ryansobol-theme.fish
@@ -0,0 +1,7 @@
+function _ryansobol_theme_install --on-event ryansobol_theme_install
+  # brew install bat
+end
+
+function _bat_uninstall --on-event ryansobol_theme_uninstall
+  # brew uninstall bat
+end
---
+++ themes/ryansobol.fish
@@ -0,0 +1,34 @@
+# name: 'Ryan Sobol Fish Theme'
+# preferred_background: 'black'
+# url: 'https://github.com/ryansobol/theme.fish'
+
+fish_color_autosuggestion '555'  'yellow'
+fish_color_cancel -r
+fish_color_command 'normal'  '--bold'
+fish_color_comment black
+fish_color_cwd blue
+fish_color_cwd_root brblack
+fish_color_end yellow
+fish_color_error 'red'  '--bold'
+fish_color_escape cyan
+fish_color_history_current cyan
+fish_color_host normal
+fish_color_host_remote yellow
+# fish_color_keyword c397d8
+fish_color_match cyan
+fish_color_normal normal
+fish_color_operator cyan
+# fish_color_option 7aa6da
+fish_color_param normal
+fish_color_quote green
+fish_color_redirection cyan
+fish_color_search_match --background=515151
+fish_color_selection --background=purple
+fish_color_status red
+fish_color_user brgreen
+fish_color_valid_path --underline
+fish_pager_color_completion cyan
+fish_pager_color_description normal
+fish_pager_color_prefix normal
+fish_pager_color_progress yellow
+fish_pager_color_selected_background -r
